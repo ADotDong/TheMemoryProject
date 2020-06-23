df09b85e9271b1fb08bce170c0e8bf00866bc6ab
ADotDong <amydong7777@gmail.com>
2020-06-23 13:56:41 -0500

    Initial Commit

---
+++ TheMemoryProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMemoryProject
+//
+//  Created by Qunfeng Dong on 6/23/20.
+//  Copyright © 2020 Amy Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
